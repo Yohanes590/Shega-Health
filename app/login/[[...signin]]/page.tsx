5380d6b155396b986a177c496ad289ef5624d096
Yohanes Mulugeta <jplussince34@gmail.com>
2025-07-30 15:15:02 +0300

    final push 1

--- app/login/[[...signin]]/page.tsx
+++ app/login/[[...signin]]/page.tsx
@@ -1,17 +1,9 @@
 "use client"
 import { SignIn } from "@clerk/nextjs";
 export default function Login() {
-const handleLogin = () => {
-  window.location.href = "/api/auth/login";
-};
       return <>
             <div className="sign-in-box h-screen w-full flex justify-center items-center">
-                  <div className="login-box-center w-[auto] h-[auto] bg-white ">
-                        <SignIn afterSignInUrl="/dashboard/home"/>
-                        <div className="login-button text-center mt-[20px]">
-                              <button onClick={handleLogin} className="w-[350px] bg-green-500 text-white cursor-pointer h-[50px] ">Login With Fayda</button>
-                        </div>
-                  </div>
+             <SignIn afterSignInUrl="/dashboard/home"/>
             </div>
             
       </>
